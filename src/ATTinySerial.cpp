55a9e7f901ad4bdabd7dccc2d291bbee598654c7
DampflokHD <73097242+DampflokHD@users.noreply.github.com>
2026-09-08 23:43:59 +0200

    Bump version to 1.0.6
    Updated library version to 1.0.6 and maintained functionality.

--- src/ATTinySerial.cpp
+++ src/ATTinySerial.cpp
@@ -2,7 +2,7 @@
  * @file ATTinySerial.cpp
  * @author DampflokHD
  * @brief Ultra-minimalist UART TX library for ATtiny microcontrollers.
- * @version 1.0.5
+ * @version 1.0.6
  * @date 2026-09-08
  * 
  * @copyright Copyright (c) 2026 DampflokHD. Licensed under the MIT License.
