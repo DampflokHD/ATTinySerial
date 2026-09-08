1a0f2ddf00c886653457c3054c92c26e74d18a47
DampflokHD <73097242+DampflokHD@users.noreply.github.com>
2026-09-08 06:16:11 +0200

    Refactor serial object name from 'serial' to 'mySerial'

--- examples/BasicTx/BasicTx.ino
+++ examples/BasicTx/BasicTx.ino
@@ -1,14 +1,14 @@
 #include <ATTinySerial.h>
 
 // Instantiate the object using default TX pin PB0
-ATTinySerial serial;
+ATTinySerial mySerial;
 
 void setup() {
     // Initialize UART at 9600 baud
-    serial.begin(9600);
+    mySerial.begin(9600);
 }
 
 void loop() {
-    serial.println("ATtiny is running.");
+    mySerial.println("ATtiny is running.");
     delay(1000);
 }
