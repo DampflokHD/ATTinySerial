f4b02dd62b0de61f19bf9c1925a3b8072e720d32
DampflokHD <73097242+DampflokHD@users.noreply.github.com>
2026-09-08 06:17:41 +0200

    Modify FlashString example for new serial setup
    Updated serial communication to use PB2 and print a string from Flash Memory.

--- examples/FlashString/FlashString.ino
+++ examples/FlashString/FlashString.ino
@@ -1,14 +1,14 @@
 #include <ATTinySerial.h>
 
-// Instantiate the object using default TX pin PB0
-ATTinySerial serial;
+ATTinySerial mySerial(2); // Set TX pin to PB2
 
 void setup() {
-    // Initialize UART at 9600 baud
-    serial.begin(9600);
+    mySerial.begin(9600);
+    
+    // The F() macro prevents the string from consuming scarce RAM
+    mySerial.println(F("This string lives in Flash Memory!"));
 }
 
 void loop() {
-    serial.println("ATtiny is running.");
-    delay(1000);
+    // Nothing to do here
 }
